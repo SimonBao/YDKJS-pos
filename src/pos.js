ac0a5dcaa401f8a3fd332b30889e6883fd553130
Simon Bao <Simonbao.sb@gmail.com>
2018-05-07 18:07:16 +0100

    Passing Test - pos_test
    POS instances have accessoryValue variable with value of 25

--- src/pos.js
+++ src/pos.js
@@ -1,7 +1,8 @@
 class POS {
-  constructor(tax = 0.20, phoneValue = 99.99, budget = 150) {
+  constructor(tax = 0.20, phoneValue = 99.99, budget = 150, accessoryValue = 25) {
     this.tax = tax;
     this.phoneValue = phoneValue;
+    this.accessoryValue = accessoryValue;
     this.budget = budget;
   }
 }
